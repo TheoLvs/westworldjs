fb2bbf0f052e71449b0a0c3ddeaddaa369df84cc
Theo Alves Da Costa <theo.alvesdacosta@ekimetrics.com>
2021-08-11 16:37:44 +0200

    Add constants and class for environments and simple grid agent

--- scripts/intro.js
+++ scripts/intro.js
@@ -2,38 +2,17 @@
 
 
 
-//Create a Pixi Application
-let app = new PIXI.Application({ 
-    width: 700,         // default: 800
-    height: 400,        // default: 600
-    antialias: true,    // default: false
-    transparent: false, // default: false
-    resolution: 1       // default: 1
-}
-);
+let env = new GridEnvironment({
+    width:40,
+    height:20,
+    objects:null,
+    cellSize:20,
+});
 
-app.renderer.backgroundColor = 0x061639;
+let agent = new BaseAgent(0,0);
+let agent2 = new BaseAgent(10,2);
 
-//Add the canvas that Pixi automatically created for you to the HTML document
-document.body.appendChild(app.view);
+env.add([agent,agent2])
 
-let type = "WebGL"
-if(!PIXI.utils.isWebGLSupported()){
-    type = "canvas"
-}
-
-PIXI.utils.sayHello(type)
-
-makeGrid(10,10,50,app.stage)
-
-makeRectangle(50,50,100,100,0x00add0,app.stage)
-
-// const line = new PIXI.Graphics();
-
-// line.lineStyle(1, 0x888888, 1); // width,color,alpha
-
-// // Draw Line
-// line.moveTo(0,0);
-// line.lineTo(100,100);
-// app.stage.addChild(line);
+env.render();
 
--- scripts/westworld.js
+++ scripts/westworld.js
@@ -1,3 +1,39 @@
+// ----------------------------------------------------------------------------------
+// WESTWORLD.js
+//
+// Working page: 
+// https://www.notion.so/Westworld-js-227c09923c2b45a28fdeeb4655e29236
+// https://github.com/TheoLvs/westworld
+
+// References:
+// https://github.com/kittykatattack/learningPixi
+// 
+// Requirements: 
+// - Pixi.js
+// ----------------------------------------------------------------------------------
+
+
+// ----------------------------------------------------------------------------------
+// Constants and utils
+// ----------------------------------------------------------------------------------
+
+
+const RED = 0xff0000
+const GREEN = 0x00ff00
+const BLUE = 0x0000ff
+const WHITE = 0xffffff
+const GRAY = 0x808080
+const YELLOW = 0xffff00
+const BLACK = 0x000000
+
+
+var RandomId = function () {
+    // Drawn from https://gist.github.com/gordonbrander/2230317
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal.
+    return '_' + Math.random().toString(36).substr(2, 9);
+  };
 
 
 // ----------------------------------------------------------------------------------
@@ -9,7 +45,7 @@ const makeRectangle = (top,left,width,height,color,container) => {
 
     const shape = new PIXI.Graphics();
     shape.beginFill(color);
-    shape.drawRect(top,left,top+height,left+width);
+    shape.drawRect(top,left,height,width);
     shape.endFill();
     container.addChild(shape);
     
@@ -28,14 +64,154 @@ const makeLine = (x1,y1,x2,y2,container,color = 0x888888,width = 1,alpha = 1) =>
 }
 
 
-const makeGrid = (width,height,cellWidth,container,color = 0x888888) => {
+const makeGrid = (width,height,cellSize,container,color = 0x888888) => {
 
     for (var i = 0 ; i < width ; i ++ ){
-        makeLine(i*cellWidth,0,i*cellWidth,1000,container,color)
+        makeLine(i*cellSize,0,i*cellSize,1000,container,color)
     }
 
     for (var j = 0 ; j < height ; j ++ ){
-        makeLine(0,j*cellWidth,1000,j*cellWidth,container,color)
+        makeLine(0,j*cellSize,1000,j*cellSize,container,color)
     }
 
 }
+
+
+
+// ----------------------------------------------------------------------------------
+// ENVIRONMENT
+// ----------------------------------------------------------------------------------
+
+
+class GridEnvironment  {
+    constructor({width = 40,height = 30,cellSize = 20,objects=null,backgroundColor=0x061639,showGrid=true,gridColor=0x888888,...params}){
+
+        // Important attributes
+        this.width = width
+        this.height = height
+        this.cellSize = cellSize
+        this._objects = {}
+
+        // Maybe better to use a container instead of directly the application
+        this.app = new PIXI.Application({ 
+            width:width * cellSize,
+            height:height * cellSize,
+            ...params,
+        });
+        this.app.renderer.backgroundColor = backgroundColor;
+
+        // Create wrapper for container
+        this.container = this.app.stage
+
+        // Create canvas
+        //Add the canvas that Pixi automatically created for you to the HTML document
+        document.body.appendChild(this.app.view);
+
+        // Add and initialize objects
+        this.add(objects)
+
+        // Display grid
+        if (showGrid){
+            makeGrid(width,height,cellSize,this.container,gridColor)
+        }
+
+
+        let type = "WebGL"
+        if(!PIXI.utils.isWebGLSupported()){
+            type = "canvas"
+        }
+
+        PIXI.utils.sayHello(type)
+
+    }
+
+
+    get objects(){
+        return Object.values(this._objects)
+    }
+
+
+    add(obj){
+        if (obj !== null){
+            if (Array.isArray(obj)){
+                obj.forEach(el => {
+                    this.add(el)
+                });
+            } else {
+                obj.bind(this)
+                this._objects[obj.id] = obj
+            }
+        }
+
+    }
+
+
+    render(){
+        this.objects.forEach(obj => {
+            obj.render()
+        })
+    }
+}
+
+
+// ----------------------------------------------------------------------------------
+// AGENTS
+// ----------------------------------------------------------------------------------
+
+
+class BaseAgent{
+    constructor(x,y,color = RED){
+
+        // Id creation
+        this.id = RandomId();
+
+        // Other attributes
+        this.x = x
+        this.y = y
+        this.color = color
+    }
+
+    bind(env){
+        this._env = env
+    }
+
+
+    get env(){
+        return this._env
+    }
+
+    get cellSize(){
+        return this._env.cellSize
+    }
+
+    get pos(){
+        return [this.x,this.y]
+    }
+
+    get top(){
+        return this.x * this.cellSize
+    }
+
+    get left(){
+        return this.y * this.cellSize
+    }
+
+    move(dx,dy){
+        self.x += dx
+        self.y += dy
+    }
+
+    render(){
+        this.renderRect()
+    }
+
+    renderRect(){
+        makeRectangle(this.top,this.left,this.cellSize,this.cellSize,this.color,this._env.container)
+    }
+
+}
+
+
+// ----------------------------------------------------------------------------------
+// OBJECTS
+// ----------------------------------------------------------------------------------
